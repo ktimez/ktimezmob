1a0ff948dc817960ea6da22a43aa4ee46c415842
Mucyo F Miller <mucyomiller@gmail.com>
2017-08-16 20:14:06 +0200

    update ga on category

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -6,6 +6,17 @@
 <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="{{mix('css/all.css')}}" rel="stylesheet">
+<!-- ga code -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-104400261-1', 'auto');
+ga('send', 'pageview');
+</script>
+<!-- end of ga -->
 </head>
 <body  style="border:1 px solid ;padding:0px;">
 <!--================ HEADER =================-->
